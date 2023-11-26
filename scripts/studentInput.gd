45f713bde21629a0962bcf3673daff309dbc6ec1
Oliwia Strzelec <strolliw@gmail.com>
2023-11-26 14:48:43 +0100

    update studentInput.gd

--- scripts/studentInput.gd
+++ scripts/studentInput.gd
@@ -7,7 +7,10 @@ var has_booster: bool
 
 var temp_speed
 
+var obstacle_instance = null
+
 func _process(_delta):
+	dig()
 	if Input.is_action_just_pressed("use_alarm"):
 		var fire_alarm_reference = get_node("../fire_alarm")
 		if fire_alarm_reference.useable and can_use_alarm:
@@ -26,7 +29,9 @@ func sabotage_alarm():
 	
 #write your logic for student here, also create obstacle scene and you can attach a script for this scene
 func dig():
-	pass
+	if Input.is_action_pressed("dig") and obstacle_instance != null:
+		if _is_student_facing_obstacle(obstacle_instance):
+			$CharacterBody2D/DiggingTimer.start()
 
 func use_terminal():
 	pass
@@ -54,7 +59,40 @@ func stop_player_movement():
 	temp_speed = $CharacterBody2D.take_current_speed_value()
 	$CharacterBody2D.stop_player_movement()
 
-
 func _on_freeze_timer_timeout():
 	#metoda, ktora po skonczeniu timera przywraca stary movement studentowi
 	$CharacterBody2D.restore_player_movement(temp_speed)
+	
+func _on_digging_timer_timeout():
+	#metoda po skończeniu DiggingTimer niszczy obstacle
+	obstacle_instance.destroy_obstacle()
+	
+func _on_student_entered_obstacle_area(obstacle):
+	#metoda przechwytuje sygnał wysłany przez obstacle jeśli student się zbliży do przeszkody
+	obstacle_instance = obstacle
+	
+func _on_student_exited_obstacle_area(body):
+	#metoda przechwytuje sygnał wysłany przez obstacle jeśli student się oddali do przeszkody
+	obstacle_instance = null
+
+func _is_student_facing_obstacle(obstacle):
+	#metoda sprawdza czy student jest zwrócony w strone obstacle
+	
+	var obstacle_position = obstacle.global_position
+	var student_position = $CharacterBody2D.global_position
+	var student_direction = $CharacterBody2D.last_direction
+
+	#Obliczamy wektor skierowany znormalizowany
+	var direction_to_obstacle = (obstacle_position - student_position).normalized()
+
+	if direction_to_obstacle.x < -0.95 and student_direction == Vector2(-1,0):
+		return true
+	elif direction_to_obstacle.x > 0.95 and student_direction == Vector2(1,0):
+		return true
+	elif direction_to_obstacle.y < -0.95 and student_direction == Vector2(0,-1):
+		return true
+	elif direction_to_obstacle.y > 0.95 and student_direction == Vector2(0,1):
+		return true
+	else:
+		return false
+
