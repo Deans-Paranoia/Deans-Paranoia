06d0bc4454f7eefba09904580185c3e509475350
Oliwia Strzelec <strolliw@gmail.com>
2023-11-26 14:47:18 +0100

    add last_direction and modify calculate_velocity()

--- scripts/movementInput.gd
+++ scripts/movementInput.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 #nazwa zmieniona z name na player_name (name w Godot to słowo kluczowe)
 var player_name: String
 
+#zmienna która pozwala określić w którą stronę zwrócony jest gracz (jego wektor przed zatrzymaniem)
+var last_direction = Vector2.ZERO
+
 #speed zostało dodane dodatkowo tymczasowo (bez tego ruch jest niezauważalny)
 var speed: int = 200
 
@@ -15,6 +18,8 @@ func apply_physics():
 	
 func calculate_velocity():
 	var direction = Input.get_vector("left","right","up","down")
+	if direction.length() > 0:
+		last_direction = direction.normalized()
 	return direction.normalized() * speed
 	
 func take_current_speed_value() -> int:
