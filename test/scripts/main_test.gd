ef278aa7dd4fb3336bea961b6ac510974efc253d
AdamRopelewski <149014250+AdamRopelewski@users.noreply.github.com>
2023-12-04 19:13:34 +0100

    abandoned mocking and UnitTesting the NetCode due to framework limitations

--- test/scripts/main_test.gd
+++ test/scripts/main_test.gd
@@ -8,15 +8,21 @@ const __source = "res://scripts/main.gd"
 
 
 func before():
-	var runner := scene_runner("res://scenes/main.tscn")
+	var main1 := scene_runner("res://scenes/main.tscn")
 	#var ServerBrowser = mock(find_child("ServerBrowser"))
-	var _ServerIp = auto_free(runner.find_child("ServerIp"))
+	var _ServerIp1 = auto_free(main1.find_child("ServerIp"))
 	#var test = ServerIp.get_property("text")
-	_ServerIp.text = "192.168.8.107"
-	runner.invoke("_on_host_button_down")
-	runner.invoke("on_start_game")
+	_ServerIp1.text = "192.168.8.107"
+	main1.invoke("_on_host_button_down")
+	await await_millis(10000)
+	main1.invoke("on_start_game")
+	#var main2 := scene_runner("res://scenes/main.tscn")
 
-	await await_millis(2000)
+
+#
+#var _ServerIp2 = auto_free(main2.find_child("ServerIp"))
+#
+#await await_millis(20000)
 
 
 func after():
