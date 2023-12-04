b17ba8b61cc3cdaa66b54ed08b03526e3485bb18
AdamRopelewski <149014250+AdamRopelewski@users.noreply.github.com>
2023-12-04 19:46:10 +0100

    added whitespaced - addon's work

--- scripts/deanInput.gd
+++ scripts/deanInput.gd
@@ -3,31 +3,39 @@ extends Node2D
 var is_tablet_open: bool = false
 # sprawdza czy wywołana została akcja "open_tablet" i wywołuje odpowiednią funkcję
 
-var can_use_alarm : bool = false
+var can_use_alarm: bool = false
 # sprawdza czy znajduje sie w strefie gdzie mozna odpalic alarm
+
 
 func _input(event):
 	# event do obslugi tabletu przez dziekana
 	if event.is_action_pressed("open_tablet"):
 		if self.name == str(multiplayer.get_unique_id()):
 			manage_deans_tablet()
-	
+
 	# event do interakcji z obiektami przez dziekana
 	if event.is_action_pressed("interaction"):
 		# obsluga alarmu
 		var fire_alarm_reference = get_node("../level/fire_alarm")
-		if fire_alarm_reference.useable and can_use_alarm and self.name == str(multiplayer.get_unique_id()):
+		if (
+			fire_alarm_reference.useable
+			and can_use_alarm
+			and self.name == str(multiplayer.get_unique_id())
+		):
 			ring_fire_alarm()
 			fire_alarm_reference.useable = false
 			change_alarm_state.rpc()
 
-@rpc("any_peer","call_remote")
-func change_alarm_state():
+
+@rpc("any_peer", "call_remote") func change_alarm_state():
 	var fire_alarm_reference = get_node("../level/fire_alarm")
-	fire_alarm_reference.useable = false	
-@rpc("any_peer","call_local")
-func remove_obstacle(_obstacle_to_destroy):
+	fire_alarm_reference.useable = false
+
+
+@rpc("any_peer", "call_local") func remove_obstacle(_obstacle_to_destroy):
 	_obstacle_to_destroy.queue_free()
+
+
 func manage_deans_tablet():
 	# funkcja do obsługi tabletu przez dziekana
 	match is_tablet_open:
@@ -38,26 +46,29 @@ func manage_deans_tablet():
 			print("Tablet closed")
 			is_tablet_open = false
 
+
 func ring_fire_alarm():
 	# funkcja do obsługi alarmu przez dziekana
 	print("Alarm rang")
-	
+
+
 func catch_student():
 	pass
 
+
 func kick_student():
 	pass
-	
+
 
 func _on_area_2d_area_entered(area):
 	#funkcja  do rejestrowanie aktualnie area, do ktorej weszlismy
 	var area_entered = area.get_name()
-	if (area_entered == "FireAlarmArea"):
+	if area_entered == "FireAlarmArea":
 		can_use_alarm = true
 
 
 func _on_area_2d_area_exited(area):
 	#funkcja  do rejestrowania aktualnie opuszczonej are
 	var area_exited = area.get_name()
-	if (area_exited == "FireAlarmArea"):
+	if area_exited == "FireAlarmArea":
 		can_use_alarm = false
