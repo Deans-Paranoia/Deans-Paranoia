30977ddd0eb37db448be31a2d1beb0396f368ed3
hoff123 <kacperxsznejder@gmail.com>
2023-12-05 20:35:54 +0100

    terminal update
    terminal update

---
+++ scripts/terminal.gd
@@ -0,0 +1,22 @@
+extends Node2D 
+
+var actual_value = 0
+var textures = []
+
+func _ready():
+	load_textures()
+	update_texture()
+	
+func load_textures():
+	for i in range(10):
+		textures.append(load("res://assets/terminal_" + str(i) + ".png"))
+
+func update_texture():
+	$Terminal/Sprite2D.texture = textures[actual_value]
+
+func use_terminal():
+	actual_value += 1
+	if actual_value > 9:
+		actual_value = 0
+	update_texture()
+
