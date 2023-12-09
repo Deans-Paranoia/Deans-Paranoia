fa1ba262e81413a7d897c56892f0091afc639630
unknown <s217418@sggw.edu.pl>
2023-12-10 00:47:18 +0100

    Stworzenie funkcji sprawdzajacej czy podano poprawny kod

--- items/server.gd
+++ items/server.gd
@@ -1,3 +1,15 @@
 extends Node2D
 
-var server_opened : bool = false
+@onready var terminal1 = get_node("terminal1")
+@onready var terminal2 = get_node("terminal2")
+@onready var terminal3 = get_node("terminal3")
+
+var value = globalScript.ServerValue
+
+func calculate_value():
+	if (100 * terminal1.actual_value + 10 * terminal2.actual_value + terminal3.actual_value) == value:
+		print("koniec gry")
+		return true
+	else:
+		print("kod niepoprawny")
+		return false
