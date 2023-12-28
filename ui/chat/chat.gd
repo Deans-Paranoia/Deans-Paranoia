3d7df394374e2d72164d9ec5d12d2cddb9497e84
GerwantZLibii <134540138+GerwantZLibii@users.noreply.github.com>
2023-12-28 14:54:53 +0100

    Added sending messages to the chat

--- ui/chat/chat.gd
+++ ui/chat/chat.gd
@@ -1,6 +1,27 @@
 extends CanvasLayer
 
-@onready var lineEdit = $Control/VBoxContainer/LineEdit
+@export var new_message: PackedScene
+
+@onready var line_edit = $Control/VBoxContainer/LineEdit
+@onready var vboxContainer = $Control/VBoxContainer
+
+var chat_in_use: bool = false
 
 func _on_student_use_chat():
-	lineEdit.grab_focus()
+	if chat_in_use:
+		if len(line_edit.text) <= 0:
+			line_edit.release_focus()
+			chat_in_use = false
+		else:
+			add_message_to_chat(line_edit.text)
+			line_edit.text = ""
+	else:
+		line_edit.grab_focus()
+		chat_in_use = true
+
+
+func add_message_to_chat(message_content):
+	var new_message_instance = new_message.instantiate()
+	vboxContainer.add_child(new_message_instance)
+	var label = new_message_instance.get_node("Label")
+	label.text = message_content
