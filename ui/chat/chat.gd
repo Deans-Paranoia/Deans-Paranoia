8e8e7c868a9fe67166ca6cc840729971ff8ea294
GerwantZLibii <134540138+GerwantZLibii@users.noreply.github.com>
2023-12-28 15:44:36 +0100

    Changed chat node structure, renamed nodes connected to chat

--- ui/chat/chat.gd
+++ ui/chat/chat.gd
@@ -2,8 +2,8 @@ extends CanvasLayer
 
 @export var new_message: PackedScene
 
-@onready var line_edit = $Control/VBoxContainer/LineEdit
-@onready var vboxContainer = $Control/VBoxContainer
+@onready var line_edit = $Panel/VBoxInput/Input
+@onready var message_panel = $Panel/VBoxMessages
 
 var chat_in_use: bool = false
 
@@ -13,6 +13,7 @@ func _on_student_use_chat():
 	if chat_in_use:
 		if len(line_edit.text) <= 0:
 			line_edit.release_focus()
+			line_edit.placeholder_text = "Press ENTER to type"
 			chat_in_use = false
 			chat_opened.emit(false)
 		else:
@@ -20,12 +21,13 @@ func _on_student_use_chat():
 			line_edit.text = ""
 	else:
 		line_edit.grab_focus()
+		line_edit.placeholder_text = ""
 		chat_in_use = true
 		chat_opened.emit(true)
 
 
 func add_message_to_chat(message_content):
 	var new_message_instance = new_message.instantiate()
-	vboxContainer.add_child(new_message_instance)
+	message_panel.add_child(new_message_instance)
 	var label = new_message_instance.get_node("Label")
 	label.text = message_content
