629b8e9099b909a30c90d42eb2b3bcb8cf5cda01
AdamRopelewski <149014250+AdamRopelewski@users.noreply.github.com>
2024-01-06 23:22:40 +0100

    Revert "fixing lines"
    This reverts commit 610c3bdea93482bc371b159e862e2ad99f87864b.

--- netcode/PlayerInfo.gd
+++ netcode/PlayerInfo.gd
@@ -1,10 +1,10 @@
-extends HBoxContainer
+extends Control
 
 signal makeAsDean(playerId)
 
 
 
 func _on_make_as_dean_button_down():
-	var playerName:String = $Name.text
+	var playerName:String = $HBoxContainer/Name.text
 	var playerId = playerName.get_slice(" ",1)
 	makeAsDean.emit(playerId)
