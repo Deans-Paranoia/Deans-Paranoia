1ed5beb302221127984924a4fa68d118f459a066
Dawid Rychlik <Dawidrychlik2004@gmail.com>
2024-01-06 21:03:49 +0100

    fixing make as dean button

--- netcode/PlayerInfo.gd
+++ netcode/PlayerInfo.gd
@@ -5,6 +5,6 @@ signal makeAsDean(playerId)
 
 
 func _on_make_as_dean_button_down():
-	var playerName:String = $Name.text
+	var playerName:String = $HBoxContainer/Name.text
 	var playerId = playerName.get_slice(" ",1)
 	makeAsDean.emit(playerId)
