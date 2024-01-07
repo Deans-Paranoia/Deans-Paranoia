17e987aad4b1b666ee84b94bface866363d4c3e3
Dawid Rychlik <Dawidrychlik2004@gmail.com>
2024-01-07 15:56:42 +0100

    adding function for running catch student animation

--- characters/student_animation_tree.gd
+++ characters/student_animation_tree.gd
@@ -3,7 +3,7 @@ extends AnimationTree
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$"../Sprite2D".set("visible", true)
+	#$"../Sprite2DWalkingStudentGirlAnimations".set("visible", true)
 	pass
 
 
@@ -16,9 +16,16 @@ func _on_character_body_2d_direction(velocity:Vector2):
 	run_animation(velocity)
 	if $"../../Timer/".is_stopped():
 		run_animation.rpc(velocity)
+		run_animation(velocity)
 		$"../../Timer".set("wait_time", 0.1)
 		$"../../Timer".start()
-
+func _on_student_catched():
+	run_catch_animation()
+	run_catch_animation.rpc()
+@rpc("any_peer","call_remote")
+func run_catch_animation():
+	$"../Sprite2D".set("visible", false)
+	$".".get("parameters/playback").travel("CatchStudentGirl")
 			
 @rpc("any_peer","call_remote")
 func run_animation(velocity):
