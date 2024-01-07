02195dc3aca5f0a433737468ee08a095b65b6295
AdamRopelewski <149014250+AdamRopelewski@users.noreply.github.com>
2024-01-07 22:47:50 +0100

    dean alarm info only for others

--- characters/deanInput.gd
+++ characters/deanInput.gd
@@ -42,7 +42,7 @@ func _input(event):
 			var fire_alarm_reference = get_node_or_null("../thirdFloor/fire_alarm")
 			if fire_alarm_reference and fire_alarm_reference.useable and can_use_alarm and self.name == str(multiplayer.get_unique_id()):
 				#display_info_alarm()
-				#display_info_alarm.rpc()
+				display_info_alarm.rpc()
 				change_alarm_state.rpc()
 	if event.is_action_pressed("catch_student"):	
 		if self.name == str(multiplayer.get_unique_id()):		
